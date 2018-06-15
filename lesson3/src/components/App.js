a27a03fc6a8e6778ff7137bf2a437caee918d9a3
JV-CORPORATION\VJ-Corporation <vf@artkaidesign.com>
2018-06-15 15:36:02 +0300

    HT3.1 range datepicker

--- lesson3/src/components/App.js
+++ lesson3/src/components/App.js
@@ -1,42 +1,126 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css' // need:  npm i style-loader css-loader --SD
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 class App extends Component {
-  static propTypes = {
-
-  };
+  /**
+   * datepicker
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleDayMouseEnter = this.handleDayMouseEnter.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
+  getInitialState() {
+    return {
+      from: null,
+      to: null,
+      enteredTo: null, // Keep track of the last day for mouseEnter.
+    };
+  }
+  isSelectingFirstDay(from, to, day) {
+    const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
+    const isRangeSelected = from && to;
+    return !from || isBeforeFirstDay || isRangeSelected;
+  }
+  handleDayClick(day) {
+    const { from, to } = this.state;
+    if (from && to && day >= from && day <= to) {
+      this.handleResetClick();
+      return;
+    }
+    if (this.isSelectingFirstDay(from, to, day)) {
+      this.setState({
+        from: day,
+        to: null,
+        enteredTo: null,
+      });
+    } else {
+      this.setState({
+        to: day,
+        enteredTo: day,
+      });
+    }
+  }
+  handleDayMouseEnter(day) {
+    const { from, to } = this.state;
+    if (!this.isSelectingFirstDay(from, to, day)) {
+      this.setState({
+        enteredTo: day,
+      });
+    }
+  }
+  handleResetClick() {
+    this.setState(this.getInitialState());
+  }
+  /*******************************************************/
 
   state = {
-      selection: null
+    selection: null
   }
 
   render() {
+    const { from, to, enteredTo } = this.state;
+    const modifiers = { start: from, end: enteredTo };
+    const disabledDays = { before: this.state.from };
+    const selectedDays = [from, { from, to: enteredTo }];
+
     const {articles} = this.props // чтоб не писать много раз this.props
     const options = this.props.articles.map(article => ({
-        label: article.title,
-        value: article.id
+      label: article.title,
+      value: article.id
     }))
     return (
       <div>
         <UserForm/>
-        <Select options = {options}
+        <Select options={options}
                 value={this.state.selection}
                 onChange={this.changeSelection}
-                //multi = {true}
+          //multi = {true}
                 multi/>
+
+        <DayPicker
+          className="Range"
+          numberOfMonths={2}
+          fromMonth={from}
+          selectedDays={selectedDays}
+          disabledDays={disabledDays}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+          onDayMouseEnter={this.handleDayMouseEnter}
+        />
+        <div>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+          to &&
+          `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from &&
+          to && (
+            <button className="link" onClick={this.handleResetClick}>
+              Reset
+            </button>
+          )}
+        </div>
+
         {/*для того чтоб при рендере открыть статью передаем id нужной статьи*/}
-        <ArticleList articles = {articles } defaultOpenId = {articles[0].id}/>
-        <ArticlesChart articles = {articles }/>
+        <ArticleList articles={articles } defaultOpenId={articles[0].id}/>
+        <ArticlesChart articles={articles }/>
       </div>
     )
   }
 
-  changeSelection = selection => this.setState({ selection })
+  changeSelection = selection => this.setState({selection})
 }
 
 export default App
